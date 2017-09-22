4f5cc71f681db66b5482b7ba1be521136af381ae
Matheus Andrade <matheus.amazonas@gmail.com>
2017-09-22 08:57:21 +0200

    Install file
    And renaming the folder

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# If Sublime was installed globally (all users), this should be its directory
+global_dir="/Applications/Sublime Text.app/Contents/MacOS/Packages"
+# If it was installed locally (current user), this should be the directory
+user_dir="~/Applications/Sublime Text.app/Contents/MacOS/Packages"
+# If it was installed in a custom directory, place it here (until the Packages folder, see above)
+custom_dir=""
+
+if [ -d "$global_dir" ]
+then
+	echo "Compressing the folder"
+    zip -r Clean.zip Clean
+    echo "Moving the file"
+    mv Clean.zip "$global_dir/Clean.sublime-package"
+    echo "Done"
+else
+	if [ -d "$user_dir" ]
+	then
+		echo "Compressing the folder"
+    	zip -r Clean.zip Clean
+    	echo "Moving the file"
+    	mv Clean.zip "$user_dir/Clean.sublime-package"
+    	echo "Done"
+	else
+		if [ -d "$custom_dir" ]
+		then
+			echo "Compressing the folder"
+	    	zip -r Clean.zip Clean
+	    	echo "Moving the file"
+	    	mv Clean.zip "$custom_dir/Clean.sublime-package"
+	    	echo "Done"
+		else
+			echo "Error: Sublime isn't installed and custom directory doesn't exist. Check the install.sh file."
+		fi
+	fi
+fi
